b6333fcbdcc6ed0347fb796afeb9e703e5ce6fbf
samrhim <rhimi1111@gmail.com>
2023-05-03 22:30:32 +0100

    Adding a docker-compose file for Flask app

--- Vagrantfile
+++ Vagrantfile
@@ -10,7 +10,7 @@ Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/focal64"
   config.vm.box_check_update = false
   config.ssh.insert_key = false
-# config.vm.synced_folder "../.", "/vagrant_data"
+  config.vm.synced_folder "./data", "/vagrant_data"
 
 # Provision docker Node
     
--- docker.sh
+++ docker.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-echo "Docker Node Preparation ..."
+
+# Setting and configuring the VM
 sudo apt-get update 
 sudo apt-get upgrade -y 
 sudo apt-get install -y python3 git vim bash-completion sshpass openssh-server curl
@@ -8,16 +9,20 @@ sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_
 systemctl restart sshd
 
 PASS=$(echo "docker" | openssl passwd -1 -stdin)
-useradd -p  "$PASS" -s /bin/bash -d /home/docker docker 
-cat <<EOF >> /etc/sudoers.d/docker
-docker	ALL=ALL   NOPASSWD: ALL
-EOF
+sudo useradd -p  "$PASS" -s /bin/bash -d /home/docker -m  docker 
+echo "docker  ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee "/etc/sudoers.d/docker"
 
-cat <<EOF >> /etc/hosts
+cat <<EOF > /etc/hosts
 192.168.1.200 docker.clevory.local
 EOF
 
+# Install docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
-su - docker -c "ssh-keygen -b 2048 -t rsa -f /home/docker/.ssh/id_rsa -q -P \"\""
+# Install docker-compose
+
+curl -SL https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
+
