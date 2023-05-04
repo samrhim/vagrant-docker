7dfddaf45b18f14f6f13c7fcf7167c1882177d29
samrhim <rhimi1111@gmail.com>
2023-05-04 12:33:26 +0100

    Modifying the Flask App

--- data/app.py
+++ data/app.py
@@ -20,4 +20,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'I have been seen {} times.\n'.format(count)
+    return 'This is a docker container, you have been seen {} times.\n'.format(count)
